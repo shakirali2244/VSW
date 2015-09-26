58ed448b8ffd6ec3b0e5bc1e68fcaaa9e0cdd48d
Kosuke <kondo1017@gmail.com>
2015-09-25 22:43:12 -0700

    Added crud on drivers and trips controller

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,2 +1,24 @@
 class DriversController < ApplicationController
+  before_action :authenticate_user!
+
+  def new
+  end
+
+  def create
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
