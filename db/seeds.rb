5fdd5425481bfeeedfb3ba9970cb9c966ac6d3cf
KoanBrown <koan.brown@gmail.com>
2015-09-26 13:23:06 -0700

    changed nav

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+10.times do
+  name = Faker::Name.name
+  location = "30 Main St"
+  Driver.create({name: name, location: location})
+end
