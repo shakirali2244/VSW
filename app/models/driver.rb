c6e33dd76a117f3b0b7a8752b62768c2bbb5ae99
Kosuke <kondo1017@gmail.com>
2015-09-25 22:24:10 -0700

    Added validations to driver and trip

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,4 +1,7 @@
 class Driver < ActiveRecord::Base
   has_many :trips, dependent: :destroy
   has_many :managers, through: :trips
+
+  validates :name, presence: true, uniqueness: true
+  validates :location, presence: true
 end
